b2da1b31c605ef13a0b7a496d96ed109564391e7
tanzil786 <tanzilidrisi68@gmail.com>
2020-09-11 02:50:50 +0530

    Initial commit

---
+++ homepage/script.js
@@ -0,0 +1,8 @@
+
+
+
+
+var nameNode = document.getElementById("name");
+nameNode.addEventListener("click", function(){
+    alert("Welcome to the world of Star Lord (Mohammad Tanzil Idrisi!)")
+});
